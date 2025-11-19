33148240517a92a0680c6a92969f01e22076c993
Gouaffo johann <jgouaffo@gmail.com>
2025-11-19 05:31:33 +0100

    documentation des api et entités

--- src/main/java/com/cwa/crud_springboot/controller/ProductController.java
+++ src/main/java/com/cwa/crud_springboot/controller/ProductController.java
@@ -2,6 +2,9 @@ package com.cwa.crud_springboot.controller;
 
 import com.cwa.crud_springboot.entity.Product;
 import com.cwa.crud_springboot.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,27 +19,64 @@ public class ProductController {
     private final ProductService service;
 
     @GetMapping
+
+    @Operation(
+            summary="Lister toute les taches",
+            description="obtenir la liste de tout les produits"
+    )
+    @ApiResponse(responseCode="200", description="Liste récupérer avec sucess")
+
     public List<Product> getAll() {
         return service.findAll();
     }
 
+
+
     @GetMapping("/{id}")
-    public Product getOne(@PathVariable Long id) {
+
+    @Operation(summary="obtenir une produit par son id")
+
+    @ApiResponses({
+            @ApiResponse(responseCode="200", description="produit ajouter"),
+            @ApiResponse(responseCode="400", description="produit non trouvé")
+    })
+
+
+    public Product getOne(@Parameter(description="id du produit") @PathVariable Long id) {
+
         return service.findById(id);
     }
+
+    @Operation(summary=" ajouter un produit")
+    @ApiResponses({
+        @ApiResponse(responseCode="200",description="produit ajouter avec success"),
+        @ApiResponse(responseCode="400",description="echec de l'ajout")
+            })
 
     @PostMapping
     public Product create(@RequestBody Product product) {
         return service.save(product);
     }
 
+    @Operation(description = "rechercher un produit par l'id")
+    @ApiResponses({
+            @ApiResponse(responseCode="200",description="produit trouvé"),
+            @ApiResponse(responseCode = "400",description = "produit itrouvable")
+    })
+
     @PutMapping("/{id}")
-    public Product update(@PathVariable Long id, @RequestBody Product product) {
+    public Product update( @Parameter(description = "id du produit que l'on recherche") @PathVariable Long id, @RequestBody Product product) {
         return service.update(id, product);
     }
 
+    @Operation(summary="suppression d'un produit")
+    @ApiResponses({
+            @ApiResponse(responseCode="200", description = "produit supprimer"),
+            @ApiResponse(responseCode= "400" ,description = "echec suppression")
+    })
+
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
+    public void delete(@Parameter(description="id du produit qu'on veut supprimer")  @PathVariable Long id) {
         service.delete(id);
     }
 }
--- src/main/java/com/cwa/crud_springboot/entity/Product.java
+++ src/main/java/com/cwa/crud_springboot/entity/Product.java
@@ -1,5 +1,6 @@
 package com.cwa.crud_springboot.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -10,9 +11,16 @@ import lombok.*;
 public class Product {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+   @Schema(description = "identifiant unique" , example="1")
     private Long id;
 
+    @Schema(description="nom de produit" , example="bonbon")
     private String name;
+
+    @Schema(description = "prix du produit", example = "1000f")
     private double price;
+
+   @Schema(description = "quantité en stock")
     private int quantity;
 }
